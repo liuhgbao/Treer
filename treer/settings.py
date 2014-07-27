e0feb7ac474381c9ebe33cd36ffa1c73d9d8ffd0
millyn <9047230@qq.com>
2014-07-27 11:26:33 +0800

    关闭debug

--- treer/settings.py
+++ treer/settings.py
@@ -23,7 +23,7 @@ sys.setdefaultencoding('utf-8')
 SECRET_KEY = '$o&emvii16h&%h8$xq=dngibw+hgm516^j6pz@v4^vk=5e^^vh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 TEMPLATE_DEBUG = True
 
